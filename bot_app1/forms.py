87bb11654727032a98ee3891fc4ff4fb94152599
Pitchuka-Kamalesh <kamalesh.4025@gmail.com>
2021-04-25 12:26:51 +0530

    model for performance created

--- bot_app1/forms.py
+++ bot_app1/forms.py
@@ -1,9 +1,16 @@
 from django import forms
-from .models import Treatment_Model
-
+from .models import Treatment_Model,PerformanceModel
+from django.conf import settings
 
 class Treatment_Form(forms.ModelForm):
     
     class Meta():
         model = Treatment_Model
+        fields = '__all__'
+
+
+class Performance_Form(forms.ModelForm):
+
+    class Meta():
+        model = PerformanceModel
         fields = '__all__'
