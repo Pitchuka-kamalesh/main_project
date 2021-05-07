d13dfe3006855f7e86b43a2f51460411229387ec
Pitchuka-Kamalesh <kamalesh.4025@gmail.com>
2021-05-07 16:11:48 +0530

    todo is added

--- bot_app1/admin.py
+++ bot_app1/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Treatment_Model,PerformanceModel
+from .models import Treatment_Model, PerformanceModel
 
-
+# todo: admin site register
 # Register your models here.
 admin.site.register(Treatment_Model)
 
